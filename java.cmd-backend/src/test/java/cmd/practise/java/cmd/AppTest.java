2b2322b48e1a06582963e3842c800288767d2aed
ridhishori <er.ridhi2016@gmail.com>
2024-03-14 22:12:28 -0400

    my first commit

---
+++ java.cmd-backend/src/test/java/cmd/practise/java/cmd/AppTest.java
@@ -0,0 +1,12 @@
+package cmd.practise.java.cmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
